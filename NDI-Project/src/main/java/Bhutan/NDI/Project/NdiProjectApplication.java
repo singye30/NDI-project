26689d30018deed8cc2a975bc149b42d7215f866
dawatsheringtamang1436 <dawatsheringtamang1436@gmail.com>
2026-08-13 11:52:30 +0600

    my commit

---
+++ NDI-Project/src/main/java/Bhutan/NDI/Project/NdiProjectApplication.java
@@ -0,0 +1,13 @@
+package Bhutan.NDI.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NdiProjectApplication.class, args);
+	}
+
+}
---
+++ NDI-Project/src/test/java/Bhutan/NDI/Project/NdiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Bhutan.NDI.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NdiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
